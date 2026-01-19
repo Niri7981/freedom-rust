174782ec966fd06adc7d8d444d4f75b2d396d34a
Niri7981 <2436663671@qq.com>
2026-01-19 21:56:23 +0800

    完成斐波那数列循环逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn fib(n:u32)->u64{
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 0..n{
+        let  next = a+b;
+        a = b;
+        b = next;
+    }
+    a
+}
+//斐波那契数列中的第n个数是后面这个循环得到的
+
+fn main(){
+    let n = 10;
+    let result  = fib(n);
+    println!("斐波那契数列的第{}个数的值是{}",n,result);
 }
